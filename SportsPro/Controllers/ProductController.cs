84b6977063b1fa38d134d5fbe11cf46cd05d3adf
Xavier Hayes <xavier.hayes@southeasttech.edu>
2024-01-19 15:20:56 -0600

    Created Product Controller Xavier H. -1/19/20224

---
+++ SportsPro/Controllers/ProductController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SportsPro.Models;
+
+namespace SportsPro.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Add()
+        {
+            ViewBag.Action = "Add";
+            return View("Edit", new Product());
+        }
+        public IActionResult ProductManager() 
+        {
+            return View();
+        
+        }
+        
+
+
+
+    }
+}
--- SportsPro/Migrations/20240118171402_Intial.Designer.cs
+++ SportsPro/Migrations/20240118171402_Intial.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SportsPro.Models;
 
@@ -11,9 +12,11 @@ using SportsPro.Models;
 namespace SportsPro.Migrations
 {
     [DbContext(typeof(SportsProContext))]
-    partial class SportsProContextModelSnapshot : ModelSnapshot
+    [Migration("20240118171402_Intial")]
+    partial class Intial
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
